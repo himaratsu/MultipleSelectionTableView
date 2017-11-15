bb6254824926db8c38875cbacc899c3b333ab581
Ryosuke Hiramatsu <himaratsu@gmail.com>
2017-11-15 10:17:57 +0900

    initial commit

--- MultipleSelectionTableView/ViewController.swift
+++ MultipleSelectionTableView/ViewController.swift
@@ -1,25 +1,40 @@
-//
-//  ViewController.swift
-//  MultipleSelectionTableView
-//
-//  Created by him on 2017/11/15.
-//  Copyright © 2017年 himara2. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.rightBarButtonItem = editButtonItem
+
+        tableView.allowsMultipleSelectionDuringEditing = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.isEditing = editing
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Item \(indexPath.row)"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("select - \(indexPath)")
+    }
+
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        print("deselect - \(indexPath)")
+    }
+}
